78de3ed1e71051235f19e1481c5f858758f08d7f
twcmeizi <53476926+twcmeizi@users.noreply.github.com>
2020-09-25 17:06:57 +0800

    新建组件
    新建组件

---
+++ App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <v-home/>
+    <h2>dfdfd</h2>
+  </div>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+export default {
+  name: "app",
+  data() {
+    return {
+     
+    };
+  },
+  components:{
+    "v-home":Home
+  },
+  methods:{
+  
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="home">
+        <h2> {{msg}}</h2>
+        <button @click="run()">run</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            msg:'home.vue'
+        }
+    },
+    methods:{
+        run(){
+            alert(this.msg)
+        }
+    }
+}
+</script>
+<style lang="scss" scoped>
+#home{
+    h2{color: red;}
+}
+</style>
+
+
+//<style lang="scss" scoped>
+//     h2{color: red;}scoped是局部作用域
+// </style>
